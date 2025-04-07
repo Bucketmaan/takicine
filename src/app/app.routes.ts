1e11eff5ffcdfe3f3eb93b1de2bb7dcb9ee8d8e2
Theo Prost <theo@prost.nom.fr>
2025-04-07 15:35:48 +0200

    add admin panel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,17 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { MoviesComponent } from './movies/movies.component';
-import { AddMovieComponent } from './movies/add-movie/add-movie.component';
-import { EditMovieComponent } from './movies/edit-movie/edit-movie.component';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {MoviesComponent} from './movies/movies.component';
+import {AddMovieComponent} from './movies/add-movie/add-movie.component';
+import {EditMovieComponent} from './movies/edit-movie/edit-movie.component';
 import {SignUpComponent} from "./sign-up/sign-up.component";
+import {PanelComponent} from "./admin/panel/panel.component";
+
 export const routes: Routes = [
-    { path: '', component: HomeComponent},
-    { path: 'movies', component: MoviesComponent},
-    { path: 'newMovie', component: AddMovieComponent},
-    { path: 'editMovie/:movieId', component: EditMovieComponent},
-    { path: 'sign-up', component: SignUpComponent}
+    {path: '', component: HomeComponent},
+    {path: 'movies', component: MoviesComponent},
+    {path: 'newMovie', component: AddMovieComponent},
+    {path: 'editMovie/:movieId', component: EditMovieComponent},
+    {path: 'sign-up', component: SignUpComponent},
+    {path: 'admin', component: PanelComponent}
 ];
 
