a1d1092befe8ee7d49fa35754034bc3cf54697e2
Hugo DALLIAS <hugo.dallias@epita.fr>
2025-04-07 15:54:33 +0200

    feat(sign-in/): Add sign-in service

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {RouterLink} from "@angular/router";
+import {UsersService} from "../services/users.service";
 import {User} from "../models/user";
 
 @Component({
@@ -11,12 +12,24 @@ import {User} from "../models/user";
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
-  user: User = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    age: 0,
-    id: undefined
-  }
+  public usersService = inject(UsersService);
+  email = '';
+  password = '';
+
+  public SignIN(): void{
+    this.usersService.getUser().subscribe((users) => {
+        for (let index = 0; index < users.length; index++) {
+
+          const element: User = users[index];
+
+          if (element.email == this.email)
+          {
+            localStorage.setItem('user_id', JSON.stringify(element.id));
+            console.log("oui");
+            break;
+          }
+          
+        }
+    })
+}
 }
