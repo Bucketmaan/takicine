66cd3aefff5ddaaa6ab72f5c79dee7fefe494ca7
Theo Prost <theo@prost.nom.fr>
2025-04-07 10:52:46 +0200

    refactor edit-movie

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,29 +1,33 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Movie } from '../models/movie';
+import {HttpClient} from '@angular/common/http';
+import {inject, Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Movie} from '../models/movie';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class MoviesService {
-  private readonly httpClient = inject(HttpClient);
-  private readonly url =  'http://localhost:8080/movies';
-  
-  getMovies(): Observable<Movie[]> {
-    return this.httpClient.get<Movie[]>(this.url);
-  }
+    private readonly httpClient = inject(HttpClient);
+    private readonly url = 'http://localhost:8080/movies';
 
-  addMovie(movie: Movie): Observable<Movie> {
-    return this.httpClient.post<Movie>(this.url, movie);
-  }
+    getMovieById(id: number): Observable<Movie> {
+        return this.httpClient.get<Movie>(`${this.url}/${id}`);
+    }
 
-  editMovie(movie: Movie): Observable<Movie> {
-    return this.httpClient.put<Movie>(`${this.url}/${movie.id}`, movie);
-  }
+    getMovies(): Observable<Movie[]> {
+        return this.httpClient.get<Movie[]>(this.url);
+    }
 
-  deleteMovie(id: number): Observable<void> { 
-    return this.httpClient.delete<void>(`${this.url}/${id}`);
-  }
+    addMovie(movie: Movie): Observable<Movie> {
+        return this.httpClient.post<Movie>(this.url, movie);
+    }
+
+    editMovie(movie: Movie): Observable<Movie> {
+        return this.httpClient.put<Movie>(`${this.url}/${movie.id}`, movie);
+    }
+
+    deleteMovie(id: number): Observable<void> {
+        return this.httpClient.delete<void>(`${this.url}/${id}`);
+    }
 
 }
