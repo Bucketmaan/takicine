ab6c3e93fe845f275848dd87c1d4cc918c1f869a
Hugo DALLIAS <hugo.dallias@epita.fr>
2025-04-07 12:50:27 +0200

    feat(home/): Add carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,14 +5,39 @@ import { Movie } from '../models/movie';
 import { AsyncPipe, DatePipe } from '@angular/common';
 import { MovieComponent } from './movie/movie.component';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AsyncPipe, DatePipe, MovieComponent, RouterLink],
+  imports: [AsyncPipe, DatePipe, MovieComponent, RouterLink, CommonModule, NgbCarouselModule, CarouselModule, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   private readonly moviesService = inject(MoviesService);
   movies$: Observable<Movie[]> = this.moviesService.getMovies();
+
+  responsiveOptions = [
+    {
+      breakpoint: '1024px',
+      numVisible: 3,
+      numScroll: 1
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 2,
+      numScroll: 1
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1,
+      numScroll: 1
+    }
+  ];
+  
+
 }
