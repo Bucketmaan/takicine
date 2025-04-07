057305fc1d44d63dbc821cf18c753b6e0e265d32
Hugo DALLIAS <hugo.dallias@epita.fr>
2025-04-07 15:46:47 +0200

    feat(sign-in/): Add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { MoviesComponent } from './movies/movies.component';
 import { AddMovieComponent } from './movies/add-movie/add-movie.component';
 import { EditMovieComponent } from './movies/edit-movie/edit-movie.component';
 import {SignUpComponent} from "./sign-up/sign-up.component";
+import {SignInComponent} from "./sign-in/sign-in.component";
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'movies', component: MoviesComponent},
     { path: 'newMovie', component: AddMovieComponent},
     { path: 'editMovie/:movieId', component: EditMovieComponent},
-    { path: 'sign-up', component: SignUpComponent}
+    { path: 'sign-up', component: SignUpComponent},
+    { path: 'sign-in', component: SignInComponent}
 ];
 
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterLink} from "@angular/router";
+import {User} from "../models/user";
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
-
+  user: User = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    age: 0,
+    id: undefined
+  }
 }
